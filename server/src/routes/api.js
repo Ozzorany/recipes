f7c7dd0c50c19e683e46cb95d96eccde6a8efc64
Oz Zorany <ozzorany@gmail.com>
2025-04-08 18:19:47 +0300

    add initial grocery CRUD

--- server/src/routes/api.js
+++ server/src/routes/api.js
@@ -3,7 +3,7 @@ const recipesRouter = require("./recipes/recipes.router");
 const usersRouter = require("./users/users.router");
 const groupsRouter = require("./groups/groups.router");
 const healthRouter = require("./health/health.router");
-
+const groceryRouter = require("./grocery/grocery.router");
 
 const admin = require("firebase-admin");
 
@@ -27,18 +27,18 @@ const verifyToken = async (req, res, next) => {
 // api.use(verifyToken);
 
 const clientMW = async (req, res, next) => {
-    if (req?.headers?.uid) {
-        req.user = {uid: req?.headers?.uid}; // Attach the decoded token to the request object
-    }
-  
-    await next();
-  };
+  if (req?.headers?.uid) {
+    req.user = { uid: req?.headers?.uid }; // Attach the decoded token to the request object
+  }
+
+  await next();
+};
 
 api.use(clientMW);
 api.use("/health", healthRouter);
 api.use("/recipes", recipesRouter);
 api.use("/users", usersRouter);
 api.use("/groups", groupsRouter);
-
+api.use("/grocery", groceryRouter);
 
 module.exports = api;
