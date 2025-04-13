2e6091df8dcd2d805e850fe60b5c21de309bccd1
Oz Zorany <ozzorany@gmail.com>
2025-04-13 15:10:19 +0300

    missing type

--- client/src/queries/mutations/useExtractGroceryItems.ts
+++ client/src/queries/mutations/useExtractGroceryItems.ts
@@ -1,6 +1,7 @@
 import { useMutation, UseMutationOptions } from "@tanstack/react-query";
 import { httpExtractGroceryItems } from "../../hooks/requests";
 import { Recipe } from "../../models/recipe.model";
+import { GroceryItem } from "../../models/grocery.model";
 
 export const useExtractGroceryItems = (
   options?: UseMutationOptions<GroceryItem[], Error, Recipe>
@@ -9,4 +10,4 @@ export const useExtractGroceryItems = (
     mutationFn: (recipe: Recipe) => httpExtractGroceryItems(recipe),
     ...options,
   });
-}; 
+};
