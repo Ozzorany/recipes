d1f20b4250e5e2f434d456cb8a6e2d257d4bc5a4
Oz Zorany <ozzorany@gmail.com>
2025-04-24 21:29:35 +0300

    remove method and ingredients from the main page cards

---
+++ client/src/components/RecipeReviewCard/RecipeReviewCard.styles.ts
@@ -0,0 +1,92 @@
+import { SxProps, Theme } from "@mui/material";
+
+export const cardStyles: SxProps<Theme> = {
+  width: "100%",
+  height: "100%",
+  display: "flex",
+  flexDirection: "column",
+  transition: "transform 0.2s ease-in-out",
+  "&:hover": {
+    transform: "translateY(-4px)",
+    boxShadow: "0 4px 20px rgba(0,0,0,0.1)",
+  },
+};
+
+export const titleStyles: SxProps<Theme> = {
+  fontSize: "1.2rem",
+  fontWeight: 500,
+  cursor: "pointer",
+  overflow: "hidden",
+  textOverflow: "ellipsis",
+  display: "-webkit-box",
+  WebkitLineClamp: 2,
+  WebkitBoxOrient: "vertical",
+  minHeight: "3.6rem",
+  "&:hover": {
+    color: "primary.main",
+  },
+};
+
+export const mediaStyles: SxProps<Theme> = {
+  height: 250,
+  width: "100%",
+  position: "relative",
+  "&::before": {
+    content: '""',
+    display: "block",
+    paddingTop: "56.25%", // 16:9 aspect ratio
+  },
+  "& img": {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    width: "100%",
+    height: "100%",
+    objectFit: "cover",
+    objectPosition: "center",
+  },
+};
+
+export const tagStyles: SxProps<Theme> = {
+  backgroundColor: "primary.light",
+  color: "white",
+  fontSize: "0.75rem",
+  height: "24px",
+  padding: "0 8px",
+  "& .MuiChip-label": {
+    padding: "0 4px",
+  },
+  "&:hover": {
+    backgroundColor: "primary.main",
+  },
+};
+
+export const likeButtonStyles: SxProps<Theme> = {
+  transition: "transform 0.2s ease-in-out",
+  "&:hover": {
+    transform: "scale(1.1)",
+  },
+};
+
+export const likeIconStyles = {
+  transition: "color 0.2s ease-in-out",
+};
+
+export const menuIconStyles: SxProps<Theme> = {
+  mr: 1,
+};
+
+export const cardContentStyles: SxProps<Theme> = {
+  flexGrow: 1,
+  display: "flex",
+  justifyContent: "start",
+};
+
+export const tagsContainerStyles: SxProps<Theme> = {
+  display: "flex",
+  flexDirection: "row",
+  flexWrap: "wrap",
+  gap: 0.5,
+  minHeight: "2.5rem",
+  alignItems: "start",
+};
--- client/src/components/RecipeReviewCard/RecipeReviewCard.tsx
+++ client/src/components/RecipeReviewCard/RecipeReviewCard.tsx
@@ -1,62 +1,41 @@
 import DeleteOutlineOutlinedIcon from "@mui/icons-material/DeleteOutlineOutlined";
-import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import FavoriteIcon from "@mui/icons-material/Favorite";
 import ModeEditOutlineOutlinedIcon from "@mui/icons-material/ModeEditOutlineOutlined";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
-import {
-  Box,
-  Checkbox,
-  Chip,
-  List,
-  ListItem,
-  ListItemButton,
-  ListItemIcon,
-  ListItemText,
-  Menu,
-  MenuItem,
-  Paper,
-  Stack,
-} from "@mui/material";
+import { Box, Chip, Menu, MenuItem, Stack } from "@mui/material";
 import Avatar from "@mui/material/Avatar";
 import Card from "@mui/material/Card";
 import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
 import CardHeader from "@mui/material/CardHeader";
 import CardMedia from "@mui/material/CardMedia";
-import Collapse from "@mui/material/Collapse";
-import IconButton, { IconButtonProps } from "@mui/material/IconButton";
+import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
 import { red } from "@mui/material/colors";
-import { styled } from "@mui/material/styles";
 import PopupState, { bindMenu, bindTrigger } from "material-ui-popup-state";
 import React, { useEffect, useRef } from "react";
 import { useNavigate } from "react-router";
 import { WhatsappIcon, WhatsappShareButton } from "react-share";
 import noImagePath from "../../assets/images/recipe-book.jpg";
-import styles from "./RecipeReviewCard.module.css"; // Import css modules stylesheet as styles
+import styles from "./RecipeReviewCard.module.css";
 import { useUserById } from "../../queries/useUserById";
 import { Recipe } from "../../models/recipe.model";
 import { auth } from "../../utils/firebase.utils";
 import { useRecipeLikesMutation } from "../../queries/mutations/useRecipeLikesMutation";
 import { useDeleteRecipe } from "../../queries/mutations/useDeleteRecipe";
-
-interface ExpandMoreProps extends IconButtonProps {
-  expand: boolean;
-}
-
-const ExpandMore = styled((props: ExpandMoreProps) => {
-  const { expand, ...other } = props;
-  return <IconButton {...other} />;
-})(({ theme, expand }) => ({
-  transform: !expand ? "rotate(0deg)" : "rotate(180deg)",
-  marginLeft: "auto",
-  transition: theme.transitions.create("transform", {
-    duration: theme.transitions.duration.shortest,
-  }),
-}));
+import {
+  cardStyles,
+  titleStyles,
+  mediaStyles,
+  tagStyles,
+  likeButtonStyles,
+  likeIconStyles,
+  menuIconStyles,
+  cardContentStyles,
+  tagsContainerStyles,
+} from "./RecipeReviewCard.styles";
 
 export default function RecipeReviewCard({ recipe }: { recipe: Recipe }) {
-  const [expanded, setExpanded] = React.useState(false);
   const [recipeLikes, setRecipeLikes] = React.useState<string[]>([]);
   const navigate = useNavigate();
   const { mutate: mutateRecipeLike } = useRecipeLikesMutation();
@@ -74,10 +53,6 @@ export default function RecipeReviewCard({ recipe }: { recipe: Recipe }) {
       setRecipeLikes(likes);
     }
   }, [likes]);
-
-  const handleExpandClick = () => {
-    setExpanded(!expanded);
-  };
 
   const handleDeleteRecipe = (popupState: any) => {
     popupState.close();
@@ -108,19 +83,17 @@ export default function RecipeReviewCard({ recipe }: { recipe: Recipe }) {
   };
 
   return (
-    <Card sx={{ width: "100%" }}>
+    <Card sx={cardStyles}>
       <CardHeader
         avatar={
-          <>
-            <Box display="flex" alignItems="center">
-              <Avatar
-                sx={{ bgcolor: red[500] }}
-                aria-label="recipe"
-                src={userLogoUrl}
-                imgProps={{ referrerPolicy: "no-referrer" }}
-              />
-            </Box>
-          </>
+          <Box display="flex" alignItems="center">
+            <Avatar
+              sx={{ bgcolor: red[500] }}
+              aria-label="recipe"
+              src={userLogoUrl}
+              imgProps={{ referrerPolicy: "no-referrer" }}
+            />
+          </Box>
         }
         action={
           <PopupState variant="popover" popupId="demo-popup-menu">
@@ -135,35 +108,25 @@ export default function RecipeReviewCard({ recipe }: { recipe: Recipe }) {
                       onClick={() => handleEditRecipe(popupState)}
                       className={styles.item}
                     >
-                      <ListItemIcon>
-                        <ModeEditOutlineOutlinedIcon />
-                      </ListItemIcon>
-                      <ListItemText>
-                        <Typography textAlign="left">עריכה</Typography>
-                      </ListItemText>
+                      <ModeEditOutlineOutlinedIcon sx={menuIconStyles} />
+                      <Typography textAlign="left">עריכה</Typography>
                     </MenuItem>
                   )}
 
                   {currentLogedInUser?.uid === recipe?.creatorId && (
                     <MenuItem onClick={() => handleDeleteRecipe(popupState)}>
-                      <ListItemIcon>
-                        <DeleteOutlineOutlinedIcon />
-                      </ListItemIcon>
-                      <ListItemText>
-                        <Typography textAlign="left">מחיקה</Typography>
-                      </ListItemText>
+                      <DeleteOutlineOutlinedIcon sx={menuIconStyles} />
+                      <Typography textAlign="left">מחיקה</Typography>
                     </MenuItem>
                   )}
                   <MenuItem>
-                    <ListItemIcon>
-                      <WhatsappShareButton
-                        url={`https://recipes-e6692.web.app/recipe/${recipe.id}`}
-                        title={recipe.description}
-                        separator=":"
-                      >
-                        <WhatsappIcon size={32} round />
-                      </WhatsappShareButton>
-                    </ListItemIcon>
+                    <WhatsappShareButton
+                      url={`https://recipes-e6692.web.app/recipe/${recipe.id}`}
+                      title={recipe.description}
+                      separator=":"
+                    >
+                      <WhatsappIcon size={32} round />
+                    </WhatsappShareButton>
                   </MenuItem>
                 </Menu>
               </React.Fragment>
@@ -171,105 +134,52 @@ export default function RecipeReviewCard({ recipe }: { recipe: Recipe }) {
           </PopupState>
         }
         title={
-          <Typography className={styles.recipeTitle}>
+          <Typography
+            className={styles.recipeTitle}
+            sx={titleStyles}
+            onClick={navigateToRecipePage}
+          >
             {recipe.description}
           </Typography>
         }
       />
-      <CardMedia
-        sx={{
-          height: 250,
-          width: "100%",
-          objectFit: recipe.image ? "cover" : "contain",
-          borderRadius: "10px",
-          cursor: "pointer",
-        }}
-        component="img"
-        image={!!recipe.image ? recipe.image : noImagePath}
-        alt={recipe.description}
-        onClick={navigateToRecipePage}
-      />
+      <Box sx={mediaStyles}>
+        <img
+          src={!!recipe.image ? recipe.image : noImagePath}
+          alt={recipe.description}
+          onClick={navigateToRecipePage}
+        />
+      </Box>
 
-      {!!recipe.ingredients.length && (
-        <Paper style={{ maxHeight: 200, overflow: "auto" }}>
-          <List sx={{ width: "100%", bgcolor: "background.paper" }}>
-            {recipe.ingredients?.map((value: any, index: number) => {
-              const labelId = `checkbox-list-label-${value}`;
-
-              return (
-                <ListItem key={index} disablePadding>
-                  <ListItemButton role={undefined} dense>
-                    <ListItemIcon>
-                      <Checkbox
-                        edge="start"
-                        tabIndex={-1}
-                        disableRipple
-                        inputProps={{ "aria-labelledby": labelId }}
-                      />
-                    </ListItemIcon>
-                    <ListItemText
-                      style={{ textAlign: "right" }}
-                      id={labelId}
-                      primary={`${value}`}
-                    />
-                  </ListItemButton>
-                </ListItem>
-              );
-            })}
-          </List>
-        </Paper>
-      )}
+      <CardContent sx={cardContentStyles}>
+        <Stack sx={tagsContainerStyles}>
+          {recipe.tags.map((tag: string) => (
+            <Chip key={tag} label={tag} size="small" sx={tagStyles} />
+          ))}
+        </Stack>
+      </CardContent>
 
       <CardActions disableSpacing>
-        <Box display="flex" alignItems="center">
+        <Box display="flex" alignItems="center" gap={1}>
           {recipeLikes?.length > 0 && (
-            <Typography>{recipeLikes?.length}</Typography>
+            <Typography variant="body2" color="text.secondary">
+              {recipeLikes?.length}
+            </Typography>
           )}
           <IconButton
             aria-label="like"
             onClick={handleLike}
-            sx={{ outline: "none !important" }}
+            sx={likeButtonStyles}
           >
             <FavoriteIcon
-              style={{ color: recipeLikes?.includes(userId) ? "red" : "gray" }}
+              style={{
+                ...likeIconStyles,
+                color: recipeLikes?.includes(userId) ? "red" : "gray",
+              }}
             />
           </IconButton>
         </Box>
-
-        {recipe.tags.map((tag: any) => {
-          return (
-            <Stack direction="row" key={tag} spacing={1}>
-              <Chip label={tag} />
-            </Stack>
-          );
-        })}
-
-        <ExpandMore
-          expand={expanded}
-          onClick={handleExpandClick}
-          aria-expanded={expanded}
-          aria-label="show more"
-        >
-          <ExpandMoreIcon />
-        </ExpandMore>
       </CardActions>
-      <Collapse in={expanded} timeout="auto" unmountOnExit>
-        <CardContent>
-          <Typography paragraph sx={{ direction: "rtl" }}>
-            :אופן ההכנה
-          </Typography>
-          <Typography
-            paragraph
-            sx={{
-              wordWrap: "break-word",
-              whiteSpace: "break-spaces",
-              textAlign: "left",
-            }}
-          >
-            {recipe.method}
-          </Typography>
-        </CardContent>
-      </Collapse>
     </Card>
   );
 }
